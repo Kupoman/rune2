a045ff2b37a94bb921932948b465826fa8cba11f
Nathan Vegdahl <cessen@cessen.com>
2015-08-18 11:36:51 -0700

    Added convenience function to merge to SourceSpans.

--- src/source_span.rs
+++ src/source_span.rs
@@ -11,7 +11,71 @@ pub struct SourceSpan<'a> {
 }
 
 impl<'a> SourceSpan<'a> {
-    pub fn text(&'a self) -> &'a str {
-        self.span
+    pub fn new_merged(s1: &SourceSpan<'a>, s2: &SourceSpan<'a>) -> SourceSpan<'a> {
+        if s1.full_source_text != s2.full_source_text {
+            panic!();
+        }
+        
+        let end_a = s1.byte_offset + s1.span.len();
+        let end_b = s2.byte_offset + s2.span.len();
+        let end = if end_a >= end_b {end_a} else {end_b};
+        
+        if s1.byte_offset <= s2.byte_offset {
+            return SourceSpan {
+                span: &s1.full_source_text[s1.byte_offset..end],
+                full_source_text: s1.full_source_text,
+                byte_offset: s1.byte_offset,
+                line: s1.line,
+                column: s1.column,
+            };
+        } else {
+            return SourceSpan {
+                span: &s1.full_source_text[s2.byte_offset..end],
+                full_source_text: s2.full_source_text,
+                byte_offset: s2.byte_offset,
+                line: s2.line,
+                column: s2.column,
+            };
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_merged() {
+        let text = "Hello there, how are\n you doing?";
+        let span1 = SourceSpan {
+            span: &text[0..22],
+            full_source_text: text,
+            byte_offset: 0,
+            line: 0,
+            column: 0,
+        };
+        let span2 = SourceSpan {
+            span: &text[23..32],
+            full_source_text: text,
+            byte_offset: 23,
+            line: 1,
+            column: 2,
+        };
+        
+        let span_merged1 = SourceSpan::new_merged(&span1, &span2);
+        let span_merged2 = SourceSpan::new_merged(&span2, &span1);
+        
+        assert_eq!(span_merged1.span, text);
+        assert_eq!(span_merged1.full_source_text, text);
+        assert_eq!(span_merged1.byte_offset, 0);
+        assert_eq!(span_merged1.line, 0);
+        assert_eq!(span_merged1.column, 0);
+        
+        assert_eq!(span_merged2.span, text);
+        assert_eq!(span_merged2.full_source_text, text);
+        assert_eq!(span_merged2.byte_offset, 0);
+        assert_eq!(span_merged2.line, 0);
+        assert_eq!(span_merged2.column, 0);
     }
 }
